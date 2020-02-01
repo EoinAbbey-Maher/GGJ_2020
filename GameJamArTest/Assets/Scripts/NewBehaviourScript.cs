4fd30e35ce703539838bc72c38e2c12d92a2fe68
johnfoley2048 <C00217207@itcarlow.ie>
2020-02-01 19:34:58 +0000

    send help

--- GameJamArTest/Assets/Scripts/NewBehaviourScript.cs
+++ GameJamArTest/Assets/Scripts/NewBehaviourScript.cs
@@ -247,4 +247,19 @@ public class NewBehaviourScript : MonoBehaviour
                 break;
         }
     }
+
+    public void xAxis(int angle)
+    {
+        gameObject.transform.Rotate(angle, 0, 0);
+    }
+
+    public void yAxis(int angle)
+    {
+        gameObject.transform.Rotate(0, angle, 0);
+    }
+
+    public void zAxis(int angle)
+    {
+        gameObject.transform.Rotate(0, 0, angle);
+    }
 }
