109d2a705f37e3c6bf741f722ce8b009a6a8ba0d
johnfoley2048 <C00217207@itcarlow.ie>
2020-02-01 17:07:32 +0000

    AR fully working

---
+++ GameJamArTest/Assets/Scripts/CubeMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMover : MonoBehaviour
+{
+    public float hSpeed;
+    public float vSpeed;
+
+    void Start()
+    {
+        hSpeed = 2.0f;
+        vSpeed = 2.0f;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float v = vSpeed * Input.GetAxis("Mouse X");
+            float h = hSpeed * Input.GetAxis("Mouse Y");
+
+            transform.Rotate(v, h, 0);
+        }
+    }
+}
